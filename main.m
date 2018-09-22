4f90dbc777f46a329e9326c6235a6e03fb52d719
JasmineJiali <37316248+JasmineJiali@users.noreply.github.com>
2018-09-22 17:15:03 +0800

    Add files via upload

--- main.m
+++ main.m
@@ -1,30 +1,28 @@
 global state;
-state = cell(65,1);
+state = cell(89,1);
 state{1,1} = [0,0,0,0,0,0,0,0,0,0,0,0,8;
       0,0,0,0,0,0,0,0,0,0,0,0,0];
   depth = 2;
-  while(depth<=65)
+  while(depth<=89)
       depth
       state_initial(depth);
-      select2(depth);
+      select(depth);
       depth = depth +1;
   end
 disp('程序运行完成，完工时间：');
 state{depth-1,1}(2,1)
 
-
-% t = cell(65,1);
-% t{65,1} = state{65,1};
-% for i = 64:-1:1
+% t = cell(89,1);
+% t{89,1} = state{89,1};
+% for i = 88:-1:1
 %     t{i,1} = state{i,t{i+1,1}(2,13)};
 % end
 % 
-% for i = 1:65
+% for i = 1:89
 %     i
 %     t{i,1}(2,1)
 %     t{i,1}(2,2)
 % end
-
 
 global from;
 global to;
@@ -35,3 +33,7 @@ to=cell(depth-2,1);
 time=cell(depth-2,1);
 R=cell(depth-2,1);
 track(depth);
+
+
+
+
